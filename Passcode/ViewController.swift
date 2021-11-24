4a4423e4f43ed456f85b1a9db4fe9d9c0d35bee1
Kyle <kylei.tw@gmail.com>
2021-11-24 21:27:22 +0800

    Initial Commit

---
+++ Passcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Passcode
+//
+//  Created by Kyle Lei on 2021/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
